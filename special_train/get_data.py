6b0e5adbc6a53ccd7c3927b6aaa13f810182974a
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-07-26 11:41:28 -0700

    Updated YAML

--- special_train/get_data.py
+++ special_train/get_data.py
@@ -14,7 +14,7 @@ from special_train.config import (
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-session = boto3.session.Session(
+session = boto3.Session(
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
     region_name=AWS_REGION,
