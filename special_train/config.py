eaa809c0c684be9c6413c414f08b90ed7089323e
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-07-30 19:41:39 -0700

    variable names typo

--- special_train/config.py
+++ special_train/config.py
@@ -1,3 +1,3 @@
 AWS_REGION = "us-east-1"
 S3_ETHEREUM_FORECAST_BUCKET = "ethereum-price-forcecast"
-S3_ETHEREUM_PRICES = "ethereum_prices_all_time.csv.gz"
+S3_ETHEREUM_PRICES = "data/ethereum_prices_all_time.csv.gz"
