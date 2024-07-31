e4998c007cb45c613da440dd3fa6a99b1d5c4d72
Zachary Raicik <zak@Zacharys-MacBook-Pro-2.local>
2024-07-30 18:59:45 -0700

    Add training dataset creation

--- special_train/get_data.py
+++ special_train/get_data.py
@@ -40,6 +40,17 @@ def get_aws_secret(SecretId):
         raise e
 
 
+def get_bucket_contents(bucket_name):
+
+    contents = aws_s3_client.list_objects_v2(Bucket=bucket_name, Prefix="data")[
+        "Contents"
+    ]
+
+    keys = [x["Key"] for x in contents]
+
+    return keys
+
+
 def create_polygon_client(api_key):
     polygon_client = RESTClient(api_key=api_key)
     return polygon_client
@@ -52,6 +63,16 @@ def get_prices(polygon_client, start_date, end_date):
 
     df = pd.DataFrame(response)
     return df
+
+
+def concat_training_data(bucket_name, keys):
+    dataframes = []
+    for key in keys:
+        response = aws_s3_client.get_object(Bucket=bucket_name, Key=key)
+        with gzip.GzipFile(fileobj=BytesIO(response["Body"].read())) as f:
+            df = pd.read_csv(f)
+            dataframes.append(df)
+    return pd.concat(dataframes, ignore_index=True).drop_duplicates(subset="timestamp")
 
 
 if __name__ == "__main__":
@@ -78,5 +99,26 @@ if __name__ == "__main__":
     aws_s3_client.put_object(
         Bucket=S3_ETHEREUM_FORECAST_BUCKET,
         Key=f"data/ethereum_prices_{(end_date - timedelta(days=7)).strftime('%Y_%m_%d')}_{end_date.strftime('%Y_%m_%d')}.csv.gz",
+        Body=gzip_buffer.getvalue(),
+    )
+
+    keys = get_bucket_contents(S3_ETHEREUM_FORECAST_BUCKET)
+
+    training_data = concat_training_data(S3_ETHEREUM_FORECAST_BUCKET, keys)
+
+    assert (
+        training_data["timestamp"].diff().iloc[1:] == 300000
+    ).all(), "Inconsistent intervals in training data"
+
+    csv_buffer = StringIO()
+    training_data.to_csv(csv_buffer, index=False)
+
+    gzip_buffer = BytesIO()
+    with gzip.GzipFile(mode="w", fileobj=gzip_buffer) as gz_file:
+        gz_file.write(csv_buffer.getvalue().encode("utf-8"))
+
+    aws_s3_client.put_object(
+        Bucket=S3_ETHEREUM_FORECAST_BUCKET,
+        Key=f"data/training_data.csv.gz",
         Body=gzip_buffer.getvalue(),
     )
